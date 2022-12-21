3425900b45ff14238fd82638240d88c3af8183e7
Lucas Pluvinage <lucas@tarides.com>
2022-12-21 19:43:22 +0100

    quick math

--- solutions/2022/D15.ml
+++ solutions/2022/D15.ml
@@ -98,24 +98,112 @@ let intersection_including_beacons ~y s =
     let stop = sx + deltaline in
     Some (start, stop)
 
+(*
+   #
+  ### => ##
+   #     ##
+
+  01234  (x+y = 2) /
+0   #
+1  ###     012
+2 ##### =>0###
+   ###    1###
+    #     2###
+
+
+cos(45) = sqrt(2)/2
+sin(45) = cos(45)
+
+x = sqrt(2) /2 (x - y)
+y = sqrt(2) /2 (x + y)
+
+(0, 2) => (-2, 2)
+(2, 0) => (2, 2)
+(4, 2) => (2, 6)
+(2, 4) => (-2, 6)
+*)
+
+module Rect = struct
+  let rot45 (x, y) = (x - y, x + y)
+  let invrot45 (x, y) = ((x + y) / 2, (y - x) / 2)
+
+  type t = { x : int; y : int; width : int; height : int }
+
+  let of_circle (x, y) d =
+    let x', y' = rot45 (x - d, y) in
+    let x'', y'' = rot45 (x, y - d) in
+    assert (y' = y'');
+    let distance = x'' - x' in
+    { x = x'; y = y'; width = distance; height = distance }
+
+  type union = t list
+
+  let invert ~bounds { x; y; width; height } =
+    (* 0123456789
+       0.........
+       1.........
+       2####XXX##
+       3.........
+       4.........
+    *)
+    let top = { x = -bounds; y = 0; width = 2 * bounds; height = y - 1 } in
+    let left = { x = -bounds; y; width = bounds + x - 1; height } in
+    let right =
+      { x = x + width + 1; y; width = bounds - (x + width + 1); height }
+    in
+    let bot =
+      {
+        x = -bounds;
+        y = y + height + 1;
+        width = 2 * bounds;
+        height = (2 * bounds) - (y + height + 1);
+      }
+    in
+    [ top; bot; left; right ]
+
+  let pp ~bounds rects =
+    let map = Array.make_matrix ((2 * bounds) + 1) ((2 * bounds) + 1) ' ' in
+    List.iter
+      (fun { x; y; width; height } ->
+        Printf.printf "%d %d => %d %d\n" x y width height;
+        for i = x to x + width do
+          for j = y to y + height do
+            try map.(j).(bounds + i) <- '#' with Invalid_argument _ -> ()
+          done
+        done)
+      rects;
+    for i = 0 to 4 * bounds do
+      Printf.printf "%s\n%!" (Array.to_seq map.(i) |> String.of_seq)
+    done
+
+  let intersect (x1a, y1a, x2a, y2a) (x1b, y1b, x2b, y2b) =
+    let x1 = max x1a x1b in
+    let y1 = max y1a y1b in
+    let x2 = min x2a x2b in
+    let y2 = min y2a y2b in
+    if x1 <= x2 && y1 <= y2 then Some (x1, y1, x2, y2) else None
+
+  let to_coor { x; y; width; height } = (x, y, x + width, y + height)
+  let of_coor (x, y, x2, y2) = { x; y; width = x2 - x; height = y2 - y }
+  let intersect a b = intersect (to_coor a) (to_coor b) |> Option.map of_coor
+  let intersect_gen lb a = List.filter_map (intersect a) lb
+  let insersect_lst la lb = List.map (intersect_gen la) lb |> List.flatten
+  let surface { width; height; _ } = (width + 1) * (height + 1)
+end
+
 let part_2 reports =
   let range = if List.length reports = 14 then 20 else 4000000 in
-  let x, y =
-    List.init range Fun.id
-    |> List.find_map (fun y ->
-           match
-             reports
-             |> List.filter_map (intersection_including_beacons ~y)
-             |> union
-             |> interval_inter (0, range)
-           with
-           | [ (0, range) ] -> None
-           | [ (a, b); (c, d) ] ->
-               assert (b + 2 = c);
-               Some (b + 1, y)
-           | _ -> failwith "no")
-    |> Option.get
+
+  let rects =
+    List.map
+      (fun { sensor; beacon } ->
+        Rect.of_circle sensor (manhattan sensor beacon))
+      reports
   in
-  (x * 4_000_000) + y
+  let lst = List.map (Rect.invert ~bounds:range) rects in
+  let instr = List.fold_left Rect.insersect_lst (List.hd lst) (List.tl lst) in
+  let v = List.find (fun v -> Rect.surface v = 1) instr in
+  let x, y = Rect.invrot45 (v.x, v.y) in
+  (x * 4000000) + y
 
 let eval = function P1 -> part_1 | P2 -> part_2
